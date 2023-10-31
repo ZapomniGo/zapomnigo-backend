31a701044e1031535d711b471283964999b98b23
Radko Mishev <rm.mishev@gmail.com>
2023-10-31 13:14:46 +0200

    Try fix

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ app.use(
 
 app.use(
   cors({
-    origin: "https://mesembria-client-e8e2f2c0ffab.herokuapp.com/",
+    origin: "https://mesembria-client-e8e2f2c0ffab.herokuapp.com",
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   })
 );
