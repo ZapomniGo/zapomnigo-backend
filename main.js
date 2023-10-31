2724fb3571a3b3a9c2504a81311805352d62ce46
Radko Mishev <rm.mishev@gmail.com>
2023-10-31 14:12:57 +0200

    Change CORS policy

--- main.js
+++ main.js
@@ -22,17 +22,21 @@ app.use(
   })
 );
 
-app.use(
-  cors({
-    origin: "https://mesembria-client-e8e2f2c0ffab.herokuapp.com",
-    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-  })
-);
+// app.use(
+//   cors({
+//     origin: "https://mesembria-client-e8e2f2c0ffab.herokuapp.com",
+//     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+//   })
+// );
+
+app.use(cors());
+
 app.use(
   bodyParser.json({
     limit: process.env.BODY_LIMIT || "5mb",
   })
 );
+app.settings("*", cors);
 app.use(express.json());
 
 app.use("/v1", authRoutes);
