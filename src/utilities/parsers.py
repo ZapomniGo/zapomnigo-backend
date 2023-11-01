4f424b6b3953cd8e5f681c3bfca68093e971a159
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2023-11-01 23:41:08 +0200

    Add dev and prod configs

---
+++ src/utilities/parsers.py
@@ -0,0 +1,9 @@
+def eval_bool(bl: str | bool) -> bool:
+    if type(bl) == bool:
+        return bl
+    else:
+        bl = bl.strip().lower()
+        if bl == "true":
+            return True
+        else:
+            return False
