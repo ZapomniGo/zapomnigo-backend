28da20472ae85391d82ad57289a370e1393a2d89
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2023-11-02 01:45:43 +0200

    Add dataclass for easier representation of objects when returning JSON

--- src/database/models/users.py
+++ src/database/models/users.py
@@ -1,11 +1,13 @@
+from dataclasses import dataclass
+
 from sqlalchemy import String, Boolean
 from sqlalchemy.orm import mapped_column, Mapped
 
 from src.database.models.base import db
 
 
+@dataclass
 class Users(db.Model):
-
     user_id: Mapped[str] = mapped_column(String(26), primary_key=True)
     username: Mapped[str] = mapped_column(String(40), nullable=False, unique=True)
     email: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
@@ -14,4 +16,3 @@ class Users(db.Model):
     gender: Mapped[str] = mapped_column(String(1), nullable=True)
     verified: Mapped[bool] = mapped_column(Boolean, default=False)
     subscription_date: Mapped[str] = mapped_column(String(40), nullable=False)
-
