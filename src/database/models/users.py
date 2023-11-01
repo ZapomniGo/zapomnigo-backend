76b5ebaba6e43357618eb7a017e5185a42caf8f5
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2023-11-02 00:47:38 +0200

    Fix types of columns

--- src/database/models/users.py
+++ src/database/models/users.py
@@ -1,3 +1,4 @@
+from sqlalchemy import String, Boolean
 from sqlalchemy.orm import mapped_column, Mapped
 
 from src.database.models.base import db
@@ -5,12 +6,12 @@ from src.database.models.base import db
 
 class Users(db.Model):
 
-    user_id: Mapped[str] = mapped_column(db.String(26), primary_key=True)
-    username: Mapped[str] = mapped_column(db.String(40), nullable=False, unique=True)
-    email: Mapped[str] = mapped_column(db.String(255), nullable=False, unique=True)
+    user_id: Mapped[str] = mapped_column(String(26), primary_key=True)
+    username: Mapped[str] = mapped_column(String(40), nullable=False, unique=True)
+    email: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
     age: Mapped[int] = mapped_column(nullable=True)
-    creation_date: Mapped[str] = mapped_column(db.String(40), nullable=False)
-    gender: Mapped[str] = mapped_column(db.String(1), nullable=True)
-    verified: Mapped[bool] = mapped_column(db.Boolean, default=False)
-    subscription_date: Mapped[str] = mapped_column(db.String(40), nullable=False)
+    creation_date: Mapped[str] = mapped_column(String(40), nullable=False)
+    gender: Mapped[str] = mapped_column(String(1), nullable=True)
+    verified: Mapped[bool] = mapped_column(Boolean, default=False)
+    subscription_date: Mapped[str] = mapped_column(String(40), nullable=False)
 
