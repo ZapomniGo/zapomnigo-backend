8d20812c81bf7867c18338eb6160844ea1637e6f
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2023-11-02 15:45:39 +0200

    Add __all__ for cleaner imports

---
+++ src/database/models/__init__.py
@@ -0,0 +1,5 @@
+from src.database.models.organizations import Organizations
+from src.database.models.subscription_models import SubscriptionModels
+from src.database.models.users import Users
+
+__all__ = ["SubscriptionModels", "Organizations", "Users"]
