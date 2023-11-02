e480f22433a681c4bbeafe9f933e671d20872ee6
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2023-11-02 16:11:13 +0200

    Add OrganizationsUsers

--- src/database/models/__init__.py
+++ src/database/models/__init__.py
@@ -1,5 +1,6 @@
 from src.database.models.organizations import Organizations
+from src.database.models.organizations_users import OrganizationsUsers
 from src.database.models.subscription_models import SubscriptionModels
 from src.database.models.users import Users
 
-__all__ = ["SubscriptionModels", "Organizations", "Users"]
+__all__ = ["SubscriptionModels", "Organizations", "Users", "OrganizationsUsers"]
