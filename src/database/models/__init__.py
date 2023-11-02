0b66a635b85b5daac0b913471457288205dce9fe
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2023-11-02 23:16:18 +0200

    Create preferences model

--- src/database/models/__init__.py
+++ src/database/models/__init__.py
@@ -4,9 +4,10 @@ from src.database.models.folders import Folders
 from src.database.models.folders_sets import FoldersSets
 from src.database.models.organizations import Organizations
 from src.database.models.organizations_users import OrganizationsUsers
+from src.database.models.preferences import Preferences
 from src.database.models.sets import Sets
 from src.database.models.subscription_models import SubscriptionModels
 from src.database.models.users import Users
 
 __all__ = ["SubscriptionModels", "Organizations", "Users", "OrganizationsUsers", "Sets", "Categories",
-           "Comments", "Folders", "FoldersSets"]
+           "Comments", "Folders", "FoldersSets", "Preferences"]
--- src/database/models/users.py
+++ src/database/models/users.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-from sqlalchemy import String, Boolean, ForeignKey
+from sqlalchemy import String, Boolean, ForeignKey, Integer
 from sqlalchemy.orm import mapped_column, Mapped, relationship
 
 from src.database.models.base import db
@@ -11,7 +11,7 @@ class Users(db.Model):
     user_id: Mapped[str] = mapped_column(String(26), primary_key=True)
     username: Mapped[str] = mapped_column(String(40), nullable=False, unique=True)
     email: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
-    age: Mapped[int] = mapped_column(nullable=True)
+    age: Mapped[int] = mapped_column(Integer, nullable=True)
     user_creation_date: Mapped[str] = mapped_column(String(40), nullable=False)
     gender: Mapped[str] = mapped_column(String(1), nullable=True)
     verified: Mapped[bool] = mapped_column(Boolean, default=False)
@@ -24,3 +24,4 @@ class Users(db.Model):
     sets: Mapped["Sets"] = relationship(back_populates="users", cascade="all")
     comments: Mapped["Comments"] = relationship(back_populates="users", cascade="all")
     folders: Mapped["Folders"] = relationship(back_populates="users", cascade="all")
+    preferences: Mapped["Preferences"] = relationship(back_populates="users", cascade="all")
