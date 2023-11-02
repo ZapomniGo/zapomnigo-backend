5a50f183abd903d14a18b92518d7ab11510f6e99
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2023-11-02 17:57:27 +0200

    Remove wrong pk

--- src/database/models/comments.py
+++ src/database/models/comments.py
@@ -9,7 +9,7 @@ from src.database.models.base import db
 @dataclass
 class Comments(db.Model):
     comment_id: Mapped[str] = mapped_column(String(26), primary_key=True)
-    comment: Mapped[str] = mapped_column(String(255), primary_key=True)
+    comment: Mapped[str] = mapped_column(String(255))
     comment_creation_date: Mapped[str] = mapped_column(String(40), nullable=False)
     user_id: Mapped[str] = mapped_column(ForeignKey("users.user_id"))
     set_id: Mapped[str] = mapped_column(ForeignKey("sets.set_id"))
