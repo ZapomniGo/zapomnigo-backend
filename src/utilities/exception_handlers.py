ddb9f546c431e877801fb208499bbb1ad2ac6225
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2023-11-03 02:13:12 +0200

    Add message to exception handler

--- src/utilities/exception_handlers.py
+++ src/utilities/exception_handlers.py
@@ -11,7 +11,8 @@ class ExceptionHandlers:
         content = {"message": "Something went wrong, please contact ZapomniGo!"}
 
         if IS_OFFLINE:
-            content = {"exception": str(exc),
+            content = {"message": "I am going to cut your legs off",
+                       "exception": str(exc),
                        "http_method": request.method,
                        "url": str(request.url),
                        "body": None if request.data == b'' else str(request.get_json()),
