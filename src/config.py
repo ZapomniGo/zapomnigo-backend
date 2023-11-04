d4818d4ef118cb05872817354b45c034116ab115
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2023-11-04 14:08:44 +0200

    Change name of env var

--- src/config.py
+++ src/config.py
@@ -17,4 +17,4 @@ class DevConfig:
 
 class ProdConfig:
     SECRET_KEY = getenv("PROD_SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = getenv("PROD_DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = getenv("DATABASE_URL")
