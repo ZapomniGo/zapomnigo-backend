9c39345d23b038be8e437b367ab0379ab8e118a7
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2023-11-07 01:19:19 +0200

    Add limits to data

--- src/utilities/json_schemas.py
+++ src/utilities/json_schemas.py
@@ -1,17 +1,19 @@
 register_schema = {
     'type': 'object',
     'properties': {
-        'name': {'type': 'string'},
-        'username': {'type': 'string'},
+        'name': {'type': 'string', "minLength": 2,
+                 "maxLength": 40},
+        'username': {'type': 'string', "minLength": 2,
+                     "maxLength": 40},
         'email': {'type': 'string', 'format': 'email'},
         'password': {'type': 'string'},
-        'age': {'type': 'integer'},
+        'age': {'type': 'integer', "minimum": 5,
+                "maximum": 99},
         'gender': {'type': 'string'},
         'privacy_policy': {'type': 'string'},
         'terms_and_conditions': {'type': 'string'},
         'marketing_consent': {'type': 'string'}
     },
-    'required': ['name', 'username', 'email', 'password', 'privacy_policy', 'terms_and_conditions',
-                 'marketing_consent'],
+    'required': ['name', "age"],
     'additionalProperties': False
 }
