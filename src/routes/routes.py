07bef4c44e3214a2de5e6e4f77015038dca26ff8
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2023-11-07 00:24:30 +0200

    Bind register endpoint

--- src/routes/routes.py
+++ src/routes/routes.py
@@ -2,11 +2,12 @@ from typing import List
 
 from flask import Blueprint, Flask
 
+from src.routes.users_routes import users_bp
 from src.routes.utility_routes import utility_bp
 
 
 class Routes:
-    _blueprints: List[Blueprint] = [utility_bp]
+    _blueprints: List[Blueprint] = [utility_bp, users_bp]
 
     @classmethod
     def register_blueprints(cls, app: Flask) -> None:
