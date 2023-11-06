d41cabad7abd9ab0e67737142f92683993f758da
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2023-11-07 00:39:09 +0200

    Add UsersContoller

--- src/controllers/__init__.py
+++ src/controllers/__init__.py
@@ -1,5 +1,7 @@
+from src.controllers.users_controller import UsersControllers
 from src.controllers.utility_controller import UtilityController
 
 __all__ = [
     "UtilityController",
+    "UsersControllers"
 ]
