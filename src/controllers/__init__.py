3de2588d286e95a528161fea4662fc69e5b97eb2
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2023-11-11 15:30:28 +0200

    Create ednpoint for adding subscription models

--- src/controllers/__init__.py
+++ src/controllers/__init__.py
@@ -1,7 +1,9 @@
+from src.controllers.subscription_models_controller import SubscriptionModelsController
 from src.controllers.users_controller import UsersControllers
 from src.controllers.utility_controller import UtilityController
 
 __all__ = [
     "UtilityController",
-    "UsersControllers"
+    "UsersControllers",
+    "SubscriptionModelsController"
 ]
--- src/pydantic_models/subscriptions_model.py
+++ src/pydantic_models/subscriptions_model.py
@@ -4,4 +4,4 @@ from pydantic import BaseModel
 
 
 class SubscriptionModel(BaseModel):
-    subscription_models: Literal["6 months", "1 month", "1 year"]
+    subscription_model: Literal["6 months", "1 month", "1 year"]
