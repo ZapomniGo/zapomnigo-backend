13c75b7f7da1364726005184e1dd66f506df4f23
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2023-11-13 17:18:49 +0200

    Create login route

--- src/routes/users_routes.py
+++ src/routes/users_routes.py
@@ -1,4 +1,4 @@
-from typing import Tuple, Dict
+from typing import Tuple, Dict, Any
 
 from flask import Blueprint
 
@@ -8,5 +8,10 @@ users_bp = Blueprint("users", __name__)
 
 
 @users_bp.post("/register")
-def register() -> Tuple[Dict[str, str], int]:
+def register() -> Tuple[Dict[str, Any], int]:
     return c.register_user()
+
+
+@users_bp.post("/login")
+def login() -> Tuple[Dict[str, Any], int]:
+    return c.login_user()
