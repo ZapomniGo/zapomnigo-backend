7b9dba103c6f539afe2d87f5950897f8d966fcee
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2023-11-15 18:53:56 +0200

    Format code

--- src/auth/jwt_decorators.py
+++ src/auth/jwt_decorators.py
@@ -8,9 +8,11 @@ def admin_required(f):
     def wrapper(*args, **kwargs):
         access_token = request.cookies.get("access_token")
         if access_token:
+
             decoded_token = jwt.decode(access_token, SECRET_KEY, algorithms=["HS256"])
             if decoded_token.get("admin"):
                 return f(*args, **kwargs)
+
             else:
                 return {"Message": "Admin privileges required."}, 403
         else:
@@ -22,9 +24,11 @@ def admin_required(f):
 def jwt_required(f):
     def wrapper(*args, **kwargs):
         access_token = request.cookies.get("access_token")
+
         if access_token:
             jwt.decode(access_token, SECRET_KEY, algorithms=["HS256"])
             return f(*args, **kwargs)
+
         else:
             return {"Message": "No auth token provided."}, 401
 
