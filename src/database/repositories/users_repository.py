4cacc935b047084d5e66d463f3c8f7ff429ac89c
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2023-11-16 00:11:11 +0200

    Add func to search for a user by ulid

--- src/database/repositories/users_repository.py
+++ src/database/repositories/users_repository.py
@@ -5,6 +5,10 @@ from src.database.models.base import db
 class UsersRepository:
 
     @classmethod
+    def get_user_by_ulid(cls, user_id: str) -> Users | None:
+        return db.session.query(Users).filter_by(user_id=user_id).first()
+
+    @classmethod
     def get_user_by_username(cls, username: str) -> Users | None:
         return db.session.query(Users).filter_by(username=username).first()
 
