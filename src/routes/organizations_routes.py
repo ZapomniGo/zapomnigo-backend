ad415c7715e6384f9fe99b19cd22856ea3142b69
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2023-11-26 22:30:52 +0200

    protect get_organization

--- src/routes/organizations_routes.py
+++ src/routes/organizations_routes.py
@@ -1,6 +1,8 @@
 from typing import Tuple, Dict, Any
 
 from flask import Blueprint
+
+from src.auth.jwt_decorators import admin_required
 from src.controllers import OrganizationsController as c
 
 organizations_bp = Blueprint("organizations", __name__)
@@ -12,5 +14,6 @@ def get_organizations() -> Tuple[Dict[str, Any], int]:
 
 
 @organizations_bp.get("/organizations/<organization_id>")
+@admin_required
 def get_organization(organization_id: str) -> Tuple[Dict[str, Any], int]:
     return c.get_organization(organization_id)
