1972018c1b0f350acf07d71f5cf8b8da6f9c823d
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2023-11-26 21:33:35 +0200

    Add rollback when an Exception occures

--- src/utilities/exception_handlers.py
+++ src/utilities/exception_handlers.py
@@ -6,6 +6,7 @@ from sqlalchemy.exc import IntegrityError
 from werkzeug.exceptions import HTTPException
 
 from src.config import IS_OFFLINE
+from src.database.models.base import db
 
 
 class ExceptionHandlers:
@@ -21,7 +22,7 @@ class ExceptionHandlers:
                    "url": str(request.url),
                    "body": None if request.data == b'' else str(request.get_json()),
                    "stacktrace": format_exc()}
-
+        db.session.rollback()
         return content, 500
 
     @classmethod
@@ -43,7 +44,6 @@ class ExceptionHandlers:
     @classmethod
     def handle_decode_error(cls, exc: DecodeError):
         return {"message": "Invalid or missing auth token."}, 499
-
 
     @classmethod
     def register_error_handlers(cls, app: Flask):
