591031c2735e28a000e80d9fe9a5d15d4a7dfbe0
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2023-12-26 22:21:04 +0200

    Change lenght of fields for sets and flashcards

--- src/pydantic_models/flashcards_model.py
+++ src/pydantic_models/flashcards_model.py
@@ -4,18 +4,18 @@ from pydantic import BaseModel, StringConstraints, ConfigDict
 
 from src.pydantic_models.common import NAME
 
-DEFINITION = Annotated[str, StringConstraints(min_length=2, max_length=255)]
+TERM_DEFINITION = Annotated[str, StringConstraints(min_length=1, max_length=16384)]
 
 
 class FlashcardsModel(BaseModel):
-    term: NAME
-    definition: DEFINITION
-    notes: Optional[DEFINITION] = None
+    term: TERM_DEFINITION
+    definition: TERM_DEFINITION
+    notes: Optional[TERM_DEFINITION] = None
     set_id: str
 
 
 class UpdateFlashcardsModel(BaseModel):
     model_config = ConfigDict(extra='forbid')
     term: Optional[NAME] = None
-    definition: Optional[DEFINITION] = None
-    notes: Optional[DEFINITION] = None
+    definition: Optional[TERM_DEFINITION] = None
+    notes: Optional[TERM_DEFINITION] = None
--- src/pydantic_models/sets_model.py
+++ src/pydantic_models/sets_model.py
@@ -4,17 +4,18 @@ from pydantic import BaseModel, StringConstraints, ConfigDict
 
 from src.pydantic_models.common import NAME
 
-SET_DESCRIPTION = Annotated[str, StringConstraints(min_length=2, max_length=255)]
+SET_DESCRIPTION = Annotated[str, StringConstraints(min_length=2, max_length=4096)]
+SET_NAME = Annotated[str, StringConstraints(min_length=1, max_length=255)]
 
 
 class SetsModel(BaseModel):
-    set_name: NAME
+    set_name: SET_NAME
     set_description: Optional[SET_DESCRIPTION] = None
     set_category: str
 
 
 class UpdateSetsModel(BaseModel):
     model_config = ConfigDict(extra='forbid')
-    set_name: Optional[NAME] = None
+    set_name: Optional[SET_NAME] = None
     set_description: Optional[SET_DESCRIPTION] = None
     set_category: Optional[str] = None
