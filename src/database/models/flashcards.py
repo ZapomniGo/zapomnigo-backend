9ae8b6eb5451655c52d7426fd7b0f421e8ce5cd0
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2023-12-28 23:46:13 +0200

    Remove previous migration as it was made from an incorrect version of the database

--- src/database/models/flashcards.py
+++ src/database/models/flashcards.py
@@ -6,9 +6,9 @@ from src.database.models.base import db
 
 class Flashcards(db.Model):
     flashcard_id: Mapped[str] = mapped_column(String(26), primary_key=True)
-    term: Mapped[str] = mapped_column(String(16384), nullable=False)
-    definition: Mapped[str] = mapped_column(String(16384), nullable=False)
-    notes: Mapped[str] = mapped_column(String(16384), nullable=True)
+    term: Mapped[str] = mapped_column(String(524288), nullable=False)
+    definition: Mapped[str] = mapped_column(String(524288), nullable=False)
+    notes: Mapped[str] = mapped_column(String(524288), nullable=True)
     set_id: Mapped[str] = mapped_column(ForeignKey("sets.set_id"), nullable=False)
 
     # Creates a bidirectional relationship between tables
