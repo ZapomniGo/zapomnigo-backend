338436316af19ee8988fd369221ce47245f7547d
Ivan Obeshkov <ivanobreshkov12@gmail.com>
2024-01-05 23:35:49 +0200

    Add 127.0.0.1 as domain cookie

--- src/controllers/users_controller.py
+++ src/controllers/users_controller.py
@@ -80,8 +80,8 @@ class UsersController:
 
         response = make_response({"message": "user logged in"}, 200)
         response.set_cookie('access_token', access_token, secure=True,
-                            samesite="None", domain="localhost")
-        response.set_cookie('refresh_token', refresh_token, httponly=True, secure=True, samesite="None",domain="localhost")
+                            samesite="None", domain="127.0.0.1")
+        response.set_cookie('refresh_token', refresh_token, httponly=True, secure=True, samesite="None",domain="127.0.0.1")
         return response
 
     @classmethod
