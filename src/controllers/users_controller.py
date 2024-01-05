c48d2326373d7bb4a6dc73fe1672593eec6627c3
Ivan Obeshkov <ivanobreshkov12@gmail.com>
2024-01-05 23:15:25 +0200

    Change domain to localhost:5173

--- src/controllers/users_controller.py
+++ src/controllers/users_controller.py
@@ -79,8 +79,9 @@ class UsersController:
         refresh_token = JwtCreation.create_refresh_jwt_token(user.username)
 
         response = make_response({"message": "user logged in"}, 200)
-        response.set_cookie('access_token', access_token, secure=True, samesite="None", domain="127.0.0.1")
-        response.set_cookie('refresh_token', refresh_token, httponly=True, secure=True, samesite="None",domain="127.0.0.1")
+        response.set_cookie('access_token', access_token, secure=True,
+                            samesite="None", domain="localhost:5173")
+        response.set_cookie('refresh_token', refresh_token, httponly=True, secure=True, samesite="None",domain="localhost:5173")
         return response
 
     @classmethod
