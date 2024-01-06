ac5b3883c9769e5fd8f9b77c2ed52cc98fada837
Ivan Obeshkov <ivanobreshkov12@gmail.com>
2024-01-06 12:32:07 +0200

    add subdomain

--- src/controllers/users_controller.py
+++ src/controllers/users_controller.py
@@ -79,8 +79,8 @@ class UsersController:
         refresh_token = JwtCreation.create_refresh_jwt_token(user.username)
         response = make_response({"message": "user logged in"}, 200)
         if not IS_OFFLINE:
-            response.set_cookie('access_token', access_token, secure=True, samesite="None", domain="herokuapp.com")
-            response.set_cookie('refresh_token', refresh_token, httponly=True, secure=True, samesite="None", domain="herokuapp.com")
+            response.set_cookie('access_token', access_token, secure=True, samesite="None", domain=".herokuapp.com")
+            response.set_cookie('refresh_token', refresh_token, httponly=True, secure=True, samesite="None", domain=".herokuapp.com")
         else:
             response.set_cookie('access_token', access_token, secure=True,
                                 samesite="None")
