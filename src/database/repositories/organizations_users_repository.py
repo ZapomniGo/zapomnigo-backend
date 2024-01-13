fa0b4631261922549e14405d9746d0b5d21e5a1e
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2024-01-13 18:29:06 +0200

    Fix query for returning organization

--- src/database/repositories/organizations_users_repository.py
+++ src/database/repositories/organizations_users_repository.py
@@ -6,4 +6,7 @@ class OrganizationsUsersRepository:
 
     @classmethod
     def get_organization_by_user_id(cls, user_id: str) -> Organizations | None:
-        return db.session.query(OrganizationsUsers).filter_by(user_id=user_id).first()
+        if result := db.session.query(OrganizationsUsers).filter_by(user_id=user_id).first():
+            return db.session.query(Organizations).filter_by(organization_id=result.organization_id).first()
+
+        return None
