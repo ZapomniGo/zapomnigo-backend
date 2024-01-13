40be27cfa1de329ff50d318898a8bac781b9d0b6
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2024-01-13 20:44:58 +0200

    Fix generating of email

--- src/controllers/utility_controller.py
+++ src/controllers/utility_controller.py
@@ -5,7 +5,7 @@ from flask import request
 from jwt import decode
 
 from src.auth.jwt_creation import JwtCreation
-from src.config import SECRET_KEY, ADMIN_USERNAME
+from src.config import SECRET_KEY, ADMIN_USERNAME, IS_OFFLINE, IS_PROD, IS_DEV
 from src.services.mailer import send_email_background_task
 
 
@@ -36,27 +36,42 @@ class UtilityController:
             return None
 
     @classmethod
-    async def send_mail_logic(cls, email: str, username: str, is_verification = True):
+    async def send_mail_logic(cls, email: str, username: str, is_verification=True):
         token = JwtCreation.create_verification_jwt(username)
         if is_verification:
-            BODY_HTML = f"""<html>
-                    <head></head>
-                    <body>
-                      <h1>Hi welcome to our app, {username}</h1>
-                      <p>Please verify your email by clicking
-                        <a href='http://localhost:5173/verify?token={token}'>here</a></p>
-                    </body>
-                    </html>
-                                """
+            body_html = cls.generate_email_body(username, token, True)
+            subject = "Добре дошъл!"
         else:
-            BODY_HTML = f"""<html>
-                    <head></head>
-                    <body>
-                      <h1>Hello, {username}</h1>
-                      <p>Please reset your password by clicking
-                        <a href='http://localhost:5173/forgot-password?token={token}'>here</a></p>
-                    </body>
-                    </html>
-                                """
+            body_html = cls.generate_email_body(username, token, False)
+            subject = "Забравена парола"
 
-        asyncio.create_task(send_email_background_task(email, "TEST", BODY_HTML))
+        asyncio.create_task(send_email_background_task(email, subject, body_html))
+
+    @classmethod
+    def generate_email_body(cls, username, token, is_verification):
+        base_url = ""
+        if IS_OFFLINE:
+            base_url = "http://localhost:5173"
+        if IS_DEV:
+            base_url = "https://dev-client-zapomnigo-192299046f7f.herokuapp.com"
+        elif IS_PROD:
+            base_url = "https://prod-client-zapomnigo-3d223494b86d.herokuapp.com"
+
+        if is_verification:
+            template_path = '../resources/email_templates/BG_VerifyEmail.html'
+            url = f'{base_url}/verify?token={token}'
+        else:
+            template_path = '../resources/email_templates/BG_ResetPassword.html'
+            url = f'{base_url}/forgot-password?token={token}'
+
+        html_content = cls.read_html_template(template_path)
+        html_content = html_content.replace("{username}", username)
+        html_content = html_content.replace("{url}", url)
+        html_content = html_content.replace("{base_url}", base_url)
+
+        return html_content
+
+    @classmethod
+    def read_html_template(cls, template_path):
+        with open(template_path, 'r') as file:
+            return file.read()
