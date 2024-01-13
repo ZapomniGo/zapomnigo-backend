e9638aafbe91518088400f81f0bfa1e3603710bc
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2024-01-13 18:28:06 +0200

    Make confidece not null

--- src/database/models/reviews_flashcards.py
+++ src/database/models/reviews_flashcards.py
@@ -8,7 +8,7 @@ class ReviewsFlashcards(db.Model):
     reviews_flashcards_id: Mapped[str] = mapped_column(String(26), primary_key=True)
     user_id: Mapped[str] = mapped_column(ForeignKey("users.user_id"))
     flashcard_id: Mapped[str] = mapped_column(ForeignKey("flashcards.flashcard_id"))
-    confidence: Mapped[int] = mapped_column(Integer, nullable=True)
+    confidence: Mapped[int] = mapped_column(Integer, nullable=False)
 
     # Creates a bidirectional relationship between tables
     users: Mapped["Users"] = relationship(back_populates="reviews_flashcards", cascade="all")
