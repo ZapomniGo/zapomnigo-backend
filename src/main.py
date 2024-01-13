b98d6cc3c48fd0dbf9f06dfee3b5540a46cef800
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2024-01-13 15:53:27 +0200

    Remove / from CORS urls

--- src/main.py
+++ src/main.py
@@ -14,8 +14,8 @@ migrate = Migrate(directory="database/migrations")
 def create_app() -> Flask:
     app = Flask(__name__, instance_relative_config=True)
     CORS(app, resources={r"/v1/*": {"origins": ["http://localhost:5173", "http://127.0.0.1:5173",
-                                                "https://dev-client-zapomnigo-192299046f7f.herokuapp.com/",
-                                                "https://prod-client-zapomnigo-3d223494b86d.herokuapp.com/"]}}
+                                                "https://dev-client-zapomnigo-192299046f7f.herokuapp.com",
+                                                "https://prod-client-zapomnigo-3d223494b86d.herokuapp.com"]}}
          , supports_credentials=True)
 
     if IS_OFFLINE:
