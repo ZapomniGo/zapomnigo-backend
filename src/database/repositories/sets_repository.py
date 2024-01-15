e726a16945ba5312e766592d1b9f692a75b8aaf5
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2024-01-15 18:56:06 +0200

    Revert "Crud folders"

--- src/database/repositories/sets_repository.py
+++ src/database/repositories/sets_repository.py
@@ -3,9 +3,10 @@ from typing import Dict, Any, List, Tuple
 
 from flask_sqlalchemy.pagination import Pagination
 from flask_sqlalchemy.query import Query
-from sqlalchemy import desc, asc, func
+from sqlalchemy import select, Row, desc, asc, func
+from ulid import ULID
 
-from src.database.models import Organizations, Categories, OrganizationsUsers, Users, FoldersSets
+from src.database.models import Organizations, Categories, Flashcards, OrganizationsUsers, Users
 from src.database.models.base import db
 from src.database.models.sets import Sets
 from src.database.repositories.users_repository import UsersRepository
@@ -27,15 +28,6 @@ class SetsRepository:
 
     @classmethod
     def _base_query(cls) -> Query:
-        """"
-        SELECT sets.set_id, sets.set_name, sets.set_description, sets.set_modification_date, c.category_name,
-        o.organization_name, f.flashcard_id, f.term, f.definition, f.notes
-        FROM sets
-            left join public.categories c on c.category_id = sets.set_category
-            inner join public.users u on u.user_id = sets.user_id
-            left join public.organizations_users ou on u.user_id = ou.user_id
-            left join public.organizations o on o.organization_id = ou.organization_id
-        """
         return db.session.query(
             Sets.set_id, Sets.set_name, Sets.set_description, Sets.set_modification_date,
             Categories.category_name, Organizations.organization_name, Users.username
@@ -49,17 +41,11 @@ class SetsRepository:
         return cls._base_query().filter(Sets.set_id == set_id).all()
 
     @classmethod
-    def _sets_in_folder_query(cls, folder_id):
-        return cls._base_query().join(FoldersSets, FoldersSets.set_id == Sets.set_id).filter(
-            FoldersSets.folder_id == folder_id)
-
-    @classmethod
     def get_all_sets(
             cls,
             page: int = 1,
             size: int = 20,
             user_id: str = "",
-            folder_id: str = "",
             sort_by_date: bool = True,
             ascending: bool = False,
     ) -> Pagination:
@@ -70,7 +56,6 @@ class SetsRepository:
             page (int): The page number to retrieve (default is 1).
             size (int): The number of sets per page (default is 20).
             user_id (str): If provided, fetch sets associated with the specified user (default is an empty string).
-            folder_id (str): If provided, fetch sets in the given folder (default is an empty string).
             sort_by_date (bool): If True (default), the sets are ordered by creation date.
             ascending (bool): If True, the sets are ordered in ascending order, else in descending order.
 
@@ -82,9 +67,6 @@ class SetsRepository:
 
         if user_id:
             query = query.filter(Users.user_id == user_id)
-
-        if folder_id:
-            query = cls._sets_in_folder_query(folder_id)
 
         if sort_by_date:
             order_by_clause = desc(func.substring(Sets.set_id, 1, 10)) if not ascending else asc(
