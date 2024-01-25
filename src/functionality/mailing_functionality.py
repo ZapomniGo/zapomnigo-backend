6e5745e08dcb795829b327ee2da07884f2e47adc
Ivan Obeshkov <ivanobreshkov12@gmail.com>
2024-01-25 17:23:41 +0200

    Remove await form async create task

--- src/functionality/mailing_functionality.py
+++ src/functionality/mailing_functionality.py
@@ -16,7 +16,7 @@ class MailingFunctionality:
             body_html = cls.generate_email_body(username, token, False)
             subject = "Забравена парола"
 
-        await asyncio.create_task(send_email_background_task(email, subject, body_html))
+        asyncio.create_task(send_email_background_task(email, subject, body_html))
 
     @classmethod
     def generate_email_body(cls, username, token, is_verification):
