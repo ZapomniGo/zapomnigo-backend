b25b7928d84c5629fd23540ea27566f86337e4bf
Ivan Obeshkov <ivanobreshkov12@gmail.com>
2024-02-13 19:03:32 +0200

    Search version 2.0

--- src/controllers/utility_controller.py
+++ src/controllers/utility_controller.py
@@ -1,7 +1,7 @@
 from typing import Tuple, Dict, Any
 
 from flask import request
-from sqlalchemy import func, or_, desc
+from sqlalchemy import func, or_, desc, and_
 from sqlalchemy.orm import joinedload
 
 from src.config import ADMIN_USERNAME
@@ -34,55 +34,58 @@ class UtilityController:
         if not search_terms:
             return {"message": "No search query provided"}, 400
 
-        query = db.session.query(Sets, Folders, Flashcards)
-
-        # Join statements based on your SQL join
-        query = query.outerjoin(Flashcards, Sets.set_id == Flashcards.set_id)
-        query = query.outerjoin(FoldersSets, Sets.set_id == FoldersSets.set_id)
-        query = query.outerjoin(Folders, Folders.folder_id == FoldersSets.folder_id)
-
-        # Use joinedload to eagerly load related entities
-        query = query.options(
+        # Query for Sets and Flashcards
+        sets_query = db.session.query(Sets, Flashcards)
+        sets_query = sets_query.outerjoin(Flashcards, Sets.set_id == Flashcards.set_id)
+        sets_query = sets_query.options(
             joinedload(Sets.categories),
             joinedload(Sets.subcategories),
             joinedload(Sets.users),
-            joinedload(Folders.categories),
-            joinedload(Folders.subcategories),
-            joinedload(Folders.users)
         )
-
-        query = query.add_columns(
+        sets_query = sets_query.add_columns(
             func.ts_rank(
                 func.to_tsvector('simple', Sets.set_name + ' ' + Sets.set_description),
                 func.plainto_tsquery('simple', search_terms)
             ).label('rank_sets'),
             func.ts_rank(
-                func.to_tsvector('simple', Folders.folder_title + ' ' + Folders.folder_description),
-                func.plainto_tsquery('simple', search_terms)
-            ).label('rank_folders'),
-            func.ts_rank(
                 func.to_tsvector('simple', Flashcards.term + ' ' + Flashcards.definition),
                 func.plainto_tsquery('simple', search_terms)
             ).label('rank_flashcards')
         )
-
-        query = query.filter(
+        sets_query = sets_query.filter(
             or_(
                 func.to_tsvector('simple', Sets.set_name + ' ' + Sets.set_description).match(search_terms),
-                func.to_tsvector('simple', Folders.folder_title + ' ' + Folders.folder_description).match(search_terms),
                 func.to_tsvector('simple', Flashcards.term + ' ' + Flashcards.definition).match(search_terms)
             )
         )
 
-        results = query.order_by(desc('rank_sets'), desc('rank_folders'), desc('rank_flashcards')).all()
+        # Query for Folders
+        folders_query = db.session.query(Folders)
+        folders_query = folders_query.options(
+            joinedload(Folders.categories),
+            joinedload(Folders.subcategories),
+            joinedload(Folders.users)
+        )
+        folders_query = folders_query.add_columns(
+            func.ts_rank(
+                func.to_tsvector('simple', Folders.folder_title + ' ' + Folders.folder_description),
+                func.plainto_tsquery('simple', search_terms)
+            ).label('rank_folders')
+        )
+        folders_query = folders_query.filter(
+            func.to_tsvector('simple', Folders.folder_title + ' ' + Folders.folder_description).match(search_terms)
+        )
 
+        # Execute queries
+        sets_results = sets_query.order_by(desc('rank_sets'), desc('rank_flashcards')).all()
+        folders_results = folders_query.order_by(desc('rank_folders')).all()
+
+        # Process and format the results
         formatted_results = {"sets": [], "folders": []}
-        unique_entities = {"sets": set(), "folders": set()}  # To track unique set_ids and folder_ids
+        unique_entities = {"sets": set(), "folders": set()}
 
-        for result in results:
-            sets_instance, folders_instance, flashcards_instance, rank_sets, rank_folders, rank_flashcards = result
-
-            # Format the results for each entity type
+        for result in sets_results:
+            sets_instance, flashcards_instance, rank_sets, rank_flashcards = result
             formatted_sets = {
                 'set_id': sets_instance.set_id,
                 'set_name': sets_instance.set_name,
@@ -95,6 +98,12 @@ class UtilityController:
                 'rank': rank_sets
             } if sets_instance and sets_instance.set_id not in unique_entities["sets"] else None
 
+            if formatted_sets:
+                formatted_results["sets"].append(formatted_sets)
+                unique_entities["sets"].add(sets_instance.set_id)
+
+        for result in folders_results:
+            folders_instance, rank_folders = result
             formatted_folders = {
                 'folder_id': folders_instance.folder_id,
                 'folder_title': folders_instance.folder_title,
@@ -106,10 +115,6 @@ class UtilityController:
                 'verified': folders_instance.verified,
                 'rank': rank_folders
             } if folders_instance and folders_instance.folder_id not in unique_entities["folders"] else None
-
-            if formatted_sets:
-                formatted_results["sets"].append(formatted_sets)
-                unique_entities["sets"].add(sets_instance.set_id)
 
             if formatted_folders:
                 formatted_results["folders"].append(formatted_folders)
