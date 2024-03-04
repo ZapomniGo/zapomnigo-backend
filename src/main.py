0dd12e02928ff3113ca533b444083bf647c964e0
Ivan Obeshkov <ivanobreshkov12@gmail.com>
2024-03-04 11:55:43 +0200

    Fix config and kill subprocesses when ctrl+c

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import ssl
 import subprocess
+import sys
 
 from flask import Flask
 from flask_cors import CORS
@@ -26,7 +27,7 @@ def create_app() -> Flask:
                                                     "https://localhost"]}}
          , supports_credentials=True)
 
-    # This is like this so that I can connect to the dev or prod db using poetry run start
+    # This is done so that I can connect to the dev or prod db using poetry run start and having IS_OFFLINE=True
     if IS_OFFLINE and not IS_PROD and not IS_DEV:
         app.config.from_object(LocalConfig)
     elif IS_PROD:
@@ -57,10 +58,16 @@ def start() -> None:
     ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
     ssl_context.load_cert_chain(certfile='data/certs/zapomnigo.crt', keyfile='data/certs/zapomnigo.key')
     # Start Celery worker and beat services
-    subprocess.Popen(
+    worker_process = subprocess.Popen(
         ["celery", "-A", "src.celery_task_queue.make_celery", "worker", "--autoscale=8,1", "--loglevel", "INFO"])
-    subprocess.Popen(["celery", "-A", "src.celery_task_queue.make_celery", "beat", "--loglevel", "INFO"])
-    create_app().run(host="0.0.0.0", port=3884, ssl_context=ssl_context)
+    beat_process = subprocess.Popen(["celery", "-A", "src.celery_task_queue.make_celery", "beat", "--loglevel", "INFO"])
+
+    try:
+        create_app().run(host="0.0.0.0", port=3884, ssl_context=ssl_context)
+    except Exception as e:
+        worker_process.terminate()
+        beat_process.terminate()
+        sys.exit(0)
 
 
 if __name__ == '__main__':
